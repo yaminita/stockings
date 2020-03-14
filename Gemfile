0803921e3d48354a8766088328d944213f3798e1
YaminaKelly <yaminita7@gmail.com>
2020-03-14 06:09:53 -0700

    Upto error handler

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ gem 'sass-rails', '>= 6'
 gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
+gem 'bootstrap', '~> 4.4', '>= 4.4.1'
+gem 'jquery-rails'
+gem 'stock_quote', '~> 3.0'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: 'pk_9fb28d0954be4853825d829ab16c5018')
+    #StockQuote::Stock.new(api_key: 'pk_9fb28d0954be4853825d829ab16c5018')
+    if params[:ticker]==""
+      @nothing = "You forgot to enter a Symbol"
+    elsif params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      if !@stock
+        @error = "Hey! That stock symbol does not exist.... Please try again"
+      end
+
+    end
+
+  end
+
+  def about
+
   end
 end
