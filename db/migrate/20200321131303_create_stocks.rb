87370a061f50b771128bbd20a5675b75e63fe132
yamina <yaminita7@gmail.com>
2020-03-21 06:27:56 -0700

    Migrated the Stock Table

--- db/migrate/20200321131303_create_stocks.rb
+++ db/migrate/20200321131303_create_stocks.rb
@@ -1,7 +1,7 @@
 class CreateStocks < ActiveRecord::Migration[6.0]
   def change
     create_table :stocks do |t|
-      t.strings :ticker
+      t.string :ticker
       t.integer :user_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_201527) do
+ActiveRecord::Schema.define(version: 2020_03_21_131303) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
