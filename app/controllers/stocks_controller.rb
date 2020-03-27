a9322b0cc7c8cc6ac0155890f254cbc72649f008
yamina <yaminita7@gmail.com>
2020-03-27 08:16:44 -0700

    Assignated by default a user id and hided the field

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,7 @@
 class StocksController < ApplicationController
   before_action :set_stock, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /stocks
   # GET /stocks.json
@@ -59,6 +61,11 @@ class StocksController < ApplicationController
       format.html { redirect_to stocks_url, notice: 'Stock was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def  correct_user
+    @ticker = current_user.stocks.find_by(id: params[:id])
+    redirect_to stocks_path, notice: "Not Authorized to edit stock" if @ticker.nil?    
   end
 
   private
